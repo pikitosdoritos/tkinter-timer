39d7fc928004b57b820ced8545ec366e6384d56a
pikitosdoritos <fastovetsn@gmail.com>
2026-06-29 22:31:16 +0300

    Adjust window size and input field width for improved UI layout

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def start():
 window = tkinter.Tk()
 
 window.title("Timer")
-window.geometry("400x400+2800+300")
+window.geometry("300x130+2800+300")
 
 label = tkinter.Label(window)
 label.pack(pady=10)
 
-input_field = tkinter.Entry(window, width=50)
+input_field = tkinter.Entry(window, width=20)
 input_field.pack(pady=10)
 
 button = tkinter.Button(window, text="Start", command=start)
